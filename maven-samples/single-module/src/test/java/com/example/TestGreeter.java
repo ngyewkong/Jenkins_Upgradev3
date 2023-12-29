290b85a6d6e20e6b995cd165ea3b593be4c61938
Ng Yew Kong <55376202+ngyewkong@users.noreply.github.com>
2023-12-29 14:31:41 +0800

    Update TestGreeter.java with a always fail test case for jenkins test pipeline

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -35,10 +35,15 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 
-    @Test
+  @Test
   public void greetShouldIncludeGreetingMessage() {
     String someone = "World, Thanks you making me Happy";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
+
+  @Test
+  public void intentionalFailTest() {
+    assertTrue(false);
   }
 }
